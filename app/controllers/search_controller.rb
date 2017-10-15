d604cadfb70b35cbf81e440427c02653b5f07578
Andy <Andydkbui@gmail.com>
2017-10-15 21:59:31 +1100

    removed recipes

--- app/controllers/search_controller.rb
+++
@@ -1,8 +0,0 @@
-def index
-  @posts = Post.all
-  if params[:search]
-    @posts = Post.search(params[:search]).order("created_at DESC")
-  else
-    @posts = Post.all.order('created_at DESC')
-  end
-end
