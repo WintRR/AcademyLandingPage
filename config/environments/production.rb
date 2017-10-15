5a6ff8a2cad500b08ae7a7da1f7556284f17eb11
Andy <Andydkbui@gmail.com>
2017-10-15 22:59:48 +1100

    SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
     # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
